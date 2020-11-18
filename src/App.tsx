3c10b6464dfd05c4040c461e7cdc8b9a1735fc4f
Junior Alves <jjalves96@gmail.com>
2020-11-18 17:10:03 -0300

    chore: add loader Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import IconFA from 'react-native-vector-icons/FontAwesome';
 
 Icon.loadFont();
+IconFA.loadFont();
 
 import Main from './pages/Main';
 
 const App: React.FC = () => {
   return (
     <>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
       <Main />
     </>
   );
