602b0692ea9f23fc83665eedcc2949d1f6bf35a4
Junior Alves <jjalves96@gmail.com>
2020-11-18 17:10:40 -0300

    chore: add saveRepository function

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -33,6 +33,8 @@ export const Input = styled.TextInput.attrs({
   background-color: #fff;
   color: #333;
   font-size: 16px;
+
+  border: 2px solid ${(props) => (props.error ? '#ff7272' : '#fff')};
 `;
 
 export const Submit = styled.TouchableOpacity`
@@ -41,4 +43,11 @@ export const Submit = styled.TouchableOpacity`
   justify-content: center;
   border-radius: 8px;
   padding: 0 14px;
+`;
+
+export const List = styled.FlatList.attrs({
+  contentContainerStyle: {paddingHorizontal: 20},
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
 `;
